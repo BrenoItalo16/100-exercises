29d8d37570be769d6ca6a2ec96275d620bad5bb9
Breno Italo <srbrenoitalo16@gmail.com>
2024-08-15 21:46:23 -0300

    Exercise 020

---
+++ exercises/020.dart
@@ -0,0 +1,21 @@
+//020 - The same teacher from the previous exercise wants to randomly draw the
+//      presentation order of the students' assignments. Write a program that
+//      reads the names of the four students and displays the sorted order.
+
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  print("Type the first name:");
+  String firstName = stdin.readLineSync()!;
+  print("Type the second name:");
+  String secondName = stdin.readLineSync()!;
+  print("Type the third name:");
+  String thirdName = stdin.readLineSync()!;
+  print("Type the fourth name:");
+  String fourthName = stdin.readLineSync()!;
+
+  List<String> names = [firstName, secondName, thirdName, fourthName];
+  names.shuffle();
+  print('The presentation order is: ${names.join(', ')}.');
+}
